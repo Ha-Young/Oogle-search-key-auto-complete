8af34e1f71cafb2a59b54dbfb53ed365ded540a2
hychoi <hayeong28@naver.com>
2020-05-30 16:56:34 +0900

    get auto complete list using by fetch with api url

--- client/index.js
+++ client/index.js
@@ -4,6 +4,21 @@ const AUTOCOMPLETE_API = "http://localhost:3000/autocomplete?keyword=";
 
 const searchField = document.getElementById("search-field");
 
+function showAutoCompleteList(arr) {
+    
+}
+
+function getAndShowAutoCompleteKey(keyword){
+    fetch(AUTOCOMPLETE_API + keyword)
+    .then(res => res.json())
+    .then(data => showAutoCompleteList(data));
+}
+
+function searchKeyInput(e){
+    // input 할때마다 autocomplete list를 얻어와서 보여준다.
+    getAndShowAutoCompleteKey(e.target.value);
+}
+
 function main() {
     console.log("DOM Content Loaded");
     // search input에 input event 추가
